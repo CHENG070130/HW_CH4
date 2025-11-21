f0c3d27de1e7a36cff9c0c2382bce8bc85c84bac
CHENG070130 <jeff.you960130@gmail.com>
2025-11-21 21:01:46 +0800

    Add files via upload

---
+++ CH4 6.11/source/main.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    int swapped;
+    for (int pass = 0; pass < n - 1; pass++) {
+        swapped = 0;
+        for (int i = 0; i < n - 1 - pass; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = 1;
+            }
+        }
+        if (!swapped) break;
+    }
+}
+
+int main() {
+    int arr[10] = { 10, 2, 8, 6, 7, 3, 5, 4, 9, 1 };
+    int n = 10;
+    bubbleSort(arr, n);
+    printf("排序後的數列：");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
